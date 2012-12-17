ecb4370b84564f641a37aaaf5ca19f0d912dd433
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2012-12-17 00:57:00 -0800

    moved execution code to root ri_dic.rb

--- lib/ri_dic.rb
+++ lib/ri_dic.rb
@@ -3,11 +3,11 @@ require "ri_dic/dictionary"
 
 module RiDic
   def self.word_match(text_word)
-    words[text_word.upcase]
+    RiDic::Dictionary.words[text_word.upcase]
   end
 
   def self.stem_match(text_word)
     text_word.upcase!
-    word_stems.each {|key, value| (return value) if text_word.match(key)}
+    RiDic::Dictionary.word_stems.each {|key, value| (return value) if text_word.match(key)}
   end  
 end
--- lib/ri_dic/version.rb
+++ lib/ri_dic/version.rb
@@ -1,3 +1,3 @@
-module RiDict
+module RiDic
   VERSION = "0.5.1"
 end
