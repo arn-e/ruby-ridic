2db31132d397d3f7f87fb715e0d32f37e7d53258
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2012-12-18 16:01:49 -0800

    added tests and code for .all_categories_in_document and .first_category_in_document'

--- lib/ridic.rb
+++ lib/ridic.rb
@@ -8,15 +8,38 @@ module RiDic
 
   def self.stem_match(text_word)
     text_word.upcase!
-    RiDic::Dictionary.word_stems.each {|key, value| (return [key,value]) if text_word.match("^#{key}")}
+    RiDic::Dictionary.word_stems.each {|key, value| (return value) if text_word.match("^#{key}")}
     nil
   end  
 
-  def self.categories_in_document(document_text, result = [])
+  def self.all_categories_in_document(document_text, result = [])
     document_text.split(' ').each do |elem| 
       word_match(elem) == nil ? result << stem_match(elem) : result << word_match(elem)
     end
     result
+  end
+
+  def self.first_category_in_document(document_text, result = [])
+    all_categories_in_document(document_text).delete_if {|i| i == nil}.each {|elem| result << [elem.first]}
+    result
+  end
+
+  def self.first_category_distribution(document_text, result = Hash.new)
+    categories = all_categories_in_document(document_text).delete_if {|i| i == nil}
+  end
+
+  private
+
+  def self.first_category(category_list)
+    category_list.first
+  end
+
+  def self.second_category(category_list)
+    category_list[1]
+  end
+
+  def self.third_category(category_list)
+    category_list[2]
   end
 
 end
--- spec/ridic_spec.rb
+++ spec/ridic_spec.rb
@@ -13,7 +13,7 @@ describe 'RiDic' do
 
   describe '.stem_match' do 
     it 'retrives word if stem found in dictionary' do
-      RiDic.stem_match('poetically').should eql(["POET", ["EXPRESSIVE_BEH", "", "EMOTIONS"]])
+      RiDic.stem_match('poetically').should eql(["EXPRESSIVE_BEH", "", "EMOTIONS"])
     end
 
     it 'returns il if stem does not exist in dictionary' do
@@ -21,19 +21,61 @@ describe 'RiDic' do
     end
   end
 
-  describe '.categories_in_document' do
+  describe '.all_categories_in_document' do
     let(:document_text) {"splendid and sublime ale critiqued and bit the harlot of an apple"}
-    let(:categories_result){RiDic.categories_in_document(document_text)}
+    let(:categories_result){RiDic.all_categories_in_document(document_text)}
     
     it 'returns an element for every word in the document' do
       categories_result.length.should eql(document_text.split.length)
     end
 
     it 'correctly evaluates the first dictionary item' do
-      categories_result.first.should eql(["SPLENDID", ["GLORY", "", "EMOTIONS"]])
+      categories_result.first.should eql(["GLORY", "", "EMOTIONS"])
+    end
+  end
+
+  describe '.first_category_in_document' do
+    let(:document_text) {"splendid endearing and cordial sublime ale whatcha callit"}
+    let(:result) {RiDic.first_category_in_document(document_text)}
+
+    it 'returns an element for each dictionary match' do
+      result.length.should eql(6)
+    end
+
+    it 'returns a collection of elements that each also contain a single element' do      
+      result.first.length.should eql(1)      
+    end
+
+    it 'correctly evaluates the first items category to be GLORY' do
+      result.first.should eql(['GLORY'])    
+    end
+
+    it 'correctly evaluates the last items category to be SOCIAL_BEHAVIOR' do
+      result[-1].should eql(['SOCIAL_BEHAVIOR'])
+    end
+  end
+
+  describe '.first_category_distribution' do
+    context 'provided with a list including dictionary words' do
+      let(:document_text) {"splendid endearing and cordial sublime ale whatcha callit"}    
+      let(:result) {RiDic.first_category_distribution(document_text)}
+
+      it 'returns an element for each dictionary match' do
+        result.length.should eql(6)
+      end
+
+      it 'evaluates the correct number of words associated with AFFECTION' do
+        # result["AFFECTION"].should eql(2)
+      end
+    end
+    
+    context 'provided a list including no dictionary words' do
+      let(:document_text) {"humpty dumpty's funky wall"}    
+
+      it '' do
+      end
     end
 
   end
-
 end
 
