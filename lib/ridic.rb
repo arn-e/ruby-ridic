4ff138d33b3d9277e104e8aec7a48654412d2ce7
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2012-12-31 19:40:43 -0500

    significant changes to core module code, added optimized private copies of several methods, reduced duplication of method calls

--- lib/ridic.rb
+++ lib/ridic.rb
@@ -3,8 +3,9 @@ require "ridic/dictionary"
 
 module RiDic
   def self.word_match(text_word)
-    dictionary_1 = RiDic::Dictionary.words[text_word.upcase]
-    dictionary_1 == nil ? RiDic::Dictionary.word_stems[text_word.upcase] : dictionary_1
+    text_word.upcase!
+    dictionary_1 = RiDic::Dictionary.words[text_word]
+    dictionary_1 == nil ? RiDic::Dictionary.word_stems[text_word] : dictionary_1
   end
 
   def self.stem_match(text_word)
@@ -26,12 +27,40 @@ module RiDic
   end
 
   def self.category_distribution(document_text, category_number = 1, result = Hash.new(0))
-    first_categories = category_in_document(sanitize(document_text), category_number).delete_if {|i| i == nil}
+    first_categories = optimized_category_in_document(sanitize(document_text.upcase), category_number).delete_if {|i| i == nil}
     first_categories.each {|elem| result[elem.first] += 1}
     sort_distribution(result)
   end
 
   private
+
+  # optimized private copies of certain public facing methods
+  # optimized for speed by removing unnecessary duplication of method calls (sanitize, upcase, etc.)
+
+  def self.optimized_word_match(text_word)
+    dictionary_1 = RiDic::Dictionary.words[text_word]
+    dictionary_1 == nil ? RiDic::Dictionary.word_stems[text_word] : dictionary_1
+  end
+
+  def self.optimized_stem_match(text_word)
+    RiDic::Dictionary.word_stems.each {|key, value| (return value) if text_word.match("^#{key}")}
+    nil
+  end  
+
+  def self.optimized_all_categories_in_document(document_text, result = [])
+    document_text.split(' ').each do |elem|
+      res = optimized_word_match(elem) 
+      res == nil ? result << optimized_stem_match(elem) : result << res
+    end
+    result
+  end
+
+  def self.optimized_category_in_document(document_text, category_number, result = [])
+    optimized_all_categories_in_document(document_text).delete_if {|i| i == nil}.each {|elem| result << [elem[category_number - 1]]}
+    result.delete_if {|i| i == [""]}
+  end
+
+  # general private methods
 
   def self.sort_distribution(distribution_set, result = Hash.new)
     distribution_set.sort_by {|key, value| value}.reverse.each {|i| result[i[0]] = i[1]}
